8f4e5d489f5827251d1df9ed4be2ad69f780c4bb
KarunyaChavan <karunyachavan84@outlook.com>
2024-11-12 12:13:10 +0530

    FINAL

--- src/Scheduling/SJF_Preemptive/PreemptiveSJF.java
+++ src/Scheduling/SJF_Preemptive/PreemptiveSJF.java
@@ -22,63 +22,53 @@ class Process {
 
 public class PreemptiveSJF {
     public static void main(String[] args) {
+        String id;
+        int n,at,bt,min_burst_time,min_burst_index,total_completed=0,current_time = 0;
+        float avg_tat=0, avg_wt=0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of processes: ");
-        int n = sc.nextInt();
-        Process[] process_queue = new Process[n];
-        int total_completed = 0, current_time = 0;
-        float avgwt = 0, avgtat = 0;
-
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter ID for Process " + (i + 1) + " : ");
-            String pid = sc.next();
-            System.out.print("Enter Arrival Time for Process " + (i + 1) + " : ");
-            int ar = sc.nextInt();
-            System.out.print("Enter Burst Time for Process " + (i + 1) + " : ");
-            int br = sc.nextInt();
-            process_queue[i] = new Process(pid, ar, br);
+        System.out.print("Enter Number of Processes : ");
+        n = sc.nextInt();
+        Process[] p_queue = new Process[n];
+        for(int i=0;i<n;i++) {
+            System.out.print("Enter Process ID for Process " + (i+1) + " : ");
+            id = sc.next();
+            System.out.print("Enter Arrival Time : ");
+            at = sc.nextInt();
+            System.out.print("Enter Burst Time : ");
+            bt = sc.nextInt();
+            p_queue[i] = new Process(id,at,bt);
         }
-
-        while (total_completed < n) {
-            int min_burst_index = n;
-            int min_burst_time = Integer.MAX_VALUE;
-
-            // Find the process with the minimum remaining burst time that has arrived
-            for (int i = 0; i < n; i++) {
-                if (process_queue[i].arrival_time <= current_time && !process_queue[i].isCompleted && process_queue[i].remaining_time < min_burst_time) {
-                    min_burst_time = process_queue[i].remaining_time;
+        while(total_completed < n) {
+            min_burst_index = n;
+            min_burst_time = Integer.MAX_VALUE;
+            for(int i=0;i<n;i++) {
+                if(p_queue[i].arrival_time <= current_time && !p_queue[i].isCompleted && p_queue[i].remaining_time < min_burst_time) {
+                    min_burst_time = p_queue[i].remaining_time;
                     min_burst_index = i;
                 }
             }
-
-            if (min_burst_index == n) {
-                current_time++;
-            } else {
-                process_queue[min_burst_index].remaining_time--;
-                current_time++;
-
-                if (process_queue[min_burst_index].remaining_time == 0) {
-                    process_queue[min_burst_index].completion_time = current_time;
-                    process_queue[min_burst_index].turn_around_time = process_queue[min_burst_index].completion_time - process_queue[min_burst_index].arrival_time;
-                    process_queue[min_burst_index].waiting_time = process_queue[min_burst_index].turn_around_time - process_queue[min_burst_index].burst_time;
-                    process_queue[min_burst_index].isCompleted = true;
-                    total_completed++;
-                    avgwt += process_queue[min_burst_index].waiting_time;
-                    avgtat += process_queue[min_burst_index].turn_around_time;
-                }
+            if(min_burst_index!=n)
+                p_queue[min_burst_index].remaining_time--;
+            current_time++;
+            if(p_queue[min_burst_index].remaining_time == 0) {
+                p_queue[min_burst_index].completion_time = current_time;
+                p_queue[min_burst_index].turn_around_time = p_queue[min_burst_index].completion_time - p_queue[min_burst_index].arrival_time;
+                p_queue[min_burst_index].waiting_time = p_queue[min_burst_index].turn_around_time - p_queue[min_burst_index].burst_time;
+                p_queue[min_burst_index].isCompleted = true;
+                total_completed++;
+                avg_tat += p_queue[min_burst_index].turn_around_time;
+                avg_wt += p_queue[min_burst_index].waiting_time;
             }
         }
-
-        System.out.println("-------------------------------------------------------------------------------------------------------------------");
-        System.out.println("Process\t\tArrival Time\tBurst Time\tCompletion Time\t\tTurnaround Time\t\tWaiting Time");
-        System.out.println("-------------------------------------------------------------------------------------------------------------------");
-
-        for (int i = 0; i < n; i++) {
-            System.out.println(process_queue[i].id + "\t\t\t\t" + process_queue[i].arrival_time + "\t\t\t\t" + process_queue[i].burst_time + "\t\t\t\t" + process_queue[i].completion_time + "\t\t\t\t" + process_queue[i].turn_around_time + "\t\t\t\t" + process_queue[i].waiting_time);
+        avg_tat /= n;
+        avg_wt /= n;
+        System.out.println("------------------------------------------------------------------------------------------------------");
+        System.out.println("Process\t\tArrival Time\tBurst Time\tCompletion Time\t\tTurnAround Time\t\tWaiting Time");
+        System.out.println("------------------------------------------------------------------------------------------------------");
+        for(int i=0;i<n;i++) {
+            System.out.println("  "+p_queue[i].id + "\t\t\t" + p_queue[i].arrival_time + "\t\t\t" + p_queue[i].burst_time + "\t\t" + p_queue[i].completion_time + "\t\t\t" + p_queue[i].turn_around_time + "\t\t\t" + p_queue[i].waiting_time);
         }
-
-        System.out.println("\nAverage Turn Around Time: " + (avgtat / n));
-        System.out.println("Average Waiting Time: " + (avgwt / n));
-        sc.close();
+        System.out.println("Average TAT : " + avg_tat);
+        System.out.println("Average WT : " + avg_wt);
     }
 }
